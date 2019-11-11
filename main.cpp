2758d5a0d01c243fb63ead00c75fd0e09892f239
Anastasia Troian <nasara.my.ten@gmail.com>
2019-11-11 18:49:13 +0200

    Check one created

--- main.cpp
+++ main.cpp
@@ -8,6 +8,25 @@ using namespace std;
 
 int main()
 {
+	int  height = 0;
+	char constraction[4];
+	cout << "Enter height: ";
+	cin >> height;
+
+	if (height < 1)
+		while (true)
+		{
+			cout << "Enter height: ";
+			cin >> height;
+			if (!cin)
+			{
+				cin.clear();
+				while (cin.get() != '\n')
+					continue;
+				continue;
+			}
+			break;
+		}
 
 	return 0;
 }
