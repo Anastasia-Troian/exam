7d3eb4aa46e2e58f186aa954edb15f964ce0ddea
Марина Кернична <MarKer@rivne.itstep.org>
2019-11-13 18:51:10 +0200

    Coin

--- main.cpp
+++ main.cpp
@@ -2,11 +2,12 @@
 
 using namespace std;
 
-/**/
+void Cash(float& money, int m1, int m2, int m3, int m4);
 
 int main()
 {
 	float money;
+	int m1 = 25, m2 = 10, m3 = 5, m4 = 1;
 
 	cout << "Enter money: ";
 	cin >> money;
@@ -27,4 +28,9 @@ int main()
 		}
 
 	return 0;
+}
+void Cash(float& money, int m1, int m2, int m3, int m4)
+{
+	int num = money * 100;
+	
 }
