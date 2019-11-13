eac5b5cc05cd86b7357f54f336d6cc24dff44e55
Марина Кернична <MarKer@rivne.itstep.org>
2019-11-13 18:52:55 +0200

    Function cash created

--- main.cpp
+++ main.cpp
@@ -32,5 +32,29 @@ int main()
 void Cash(float& money, int m1, int m2, int m3, int m4)
 {
 	int num = money * 100;
-	
+	int ans1, ans2, ans3, ans4;
+	if (num >= m1)
+	{
+		ans1 = num / m1;
+		num %= m1;
+		cout << ans1 << " to " << m1 << " cents.\n";
+	}
+	if (num >= m2)
+	{
+		ans2 = num / m2;
+		num %= m2;
+		cout << ans2 << " to " << m2 << " cents.\n";
+	}
+	if (num >= m3)
+	{
+		ans3 = num / m3;
+		num %= m3;
+		cout << ans3 << " to " << m3 << " cents.\n";
+	}
+	if (num >= m4)
+	{
+		ans4 = num / m4;
+		num %= m4;
+		cout << ans4 << " to " << m4 << " cents.\n";
+	}
 }
